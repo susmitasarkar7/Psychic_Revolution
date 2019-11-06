96577ca035831364b01ca84b570037034d976a20
susmitasarkar7 <sarkarsusmita253@gmail.com>
2019-11-06 16:36:10 +0530

    level 2 design done level 3 is in progress

--- assets/js/level_1.js
+++ assets/js/level_1.js
@@ -1,26 +1,15 @@
 $(document).ready(function() {
     $(".circle").click(function() {
         // All Elements Animation after clicking the current level
-        $("canvas").css("transform", "scale(1.5)").fadeOut(500);
+        $("canvas").css("transform", "scale(2.5)").fadeOut(500);
         $(".circle").fadeOut();
-        $("canvas").css("transform", "scale(1.2)").fadeIn();
         $(".level-1-game").css({
             "display": "flex",
             "align-items": "center",
             "justify-content": "center",
         });
-        if($("div.intro-banner-vdo-play-btn.pinkBg").hasClass(".intro-banner-vdo-play-btn.pinkBg")){
+        if($("div.intro-banner-vdo-play-btn").hasClass(".intro-banner-vdo-play-btn")){
             console.log("foo");
-            
-            // All Elements Animation after clicking the current level
-            $("canvas").css("transform", "scale(1.5)").fadeOut(500);
-            $(".circle").fadeOut();
-            $("canvas").css("transform", "scale(1.2)").fadeIn();
-            $(".level-1-game").css({
-                "display": "flex",
-                "align-items": "center",
-                "justify-content": "center",
-            });
 
             // Highlighting the next level after completing the current level
             // $("li.ball.bubble").removeClass("ball bubble").next().addClass("ball bubble");
