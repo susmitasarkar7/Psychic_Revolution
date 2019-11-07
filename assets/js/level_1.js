8d9afc6a037a0c753ba3d50e200496d037c43cc3
susmitasarkar7 <sarkarsusmita253@gmail.com>
2019-11-07 13:35:43 +0530

    initial design complete for sirst 6 levels and working on the doors between doors and elements level

--- assets/js/level_1.js
+++ assets/js/level_1.js
@@ -1,27 +1,24 @@
 $(document).ready(function() {
-    $(".circle-position").click(function() {
+    $(".circle").click(function() {
         // All Elements Animation after clicking the current level
-        // $("canvas").css("transform", "scale(2.5)").fadeOut(500);
-        // $(".circle").fadeOut();
-        // $(".level-1-game").css({
-        //     "display": "flex",
-        //     "align-items": "center",
-        //     "justify-content": "center",
-        // });
+        $("canvas").css("transform", "scale(2.5)").fadeOut(500);
+        $(".circle").css("display", "none");
+        $(".level-1-game").css({
+            "display": "block",
+        });
         if($("div.intro-banner-vdo-play-btn.pinkBg").hasClass("intro-banner-vdo-play-btn pinkBg")){
-            console.log("foo");
 
             // Highlighting the next level after completing the current level
-            $("div.intro-banner-vdo-play-btn.pinkBg").removeClass("intro-banner-vdo-play-btn pinkBg");
-            
-            $(this).next().closest('.circle-position').children('.circle').addClass("intro-banner-vdo-play-btn pinkBg").prepend("<span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span>");
-            console.log("bar");
+            // $("div.intro-banner-vdo-play-btn.pinkBg").removeClass("intro-banner-vdo-play-btn pinkBg");
+
+            // $(this).next().closest('.circle-position').children('.circle').addClass("intro-banner-vdo-play-btn pinkBg").prepend("<span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span>");
             
 
         } else {
             // Initial stage after reloads
-            // $("li.ball.bubble").removeClass("ball bubble");
-            // $("li:first").addClass("ball bubble");
+            // $("div.intro-banner-vdo-play-btn.pinkBg").removeClass("intro-banner-vdo-play-btn pinkBg");
+
+            // $(this).next().closest('.circle-position').children('.circle').addClass("intro-banner-vdo-play-btn pinkBg").prepend("<span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span>");
         }
     });
 });
