c891c19f16023203110a654312da2d8de41f8372
susmitasarkar7 <sarkarsusmita253@gmail.com>
2019-11-07 11:58:59 +0530

    next element ripple animation done

--- assets/js/level_1.js
+++ assets/js/level_1.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
-    $(".circle").click(function() {
+    $(".circle-position").click(function() {
         // All Elements Animation after clicking the current level
-        $("canvas").css("transform", "scale(2.5)").fadeOut(500);
-        $(".circle").fadeOut();
-        $(".level-1-game").css({
-            "display": "flex",
-            "align-items": "center",
-            "justify-content": "center",
-        });
-        if($("div.intro-banner-vdo-play-btn").hasClass(".intro-banner-vdo-play-btn")){
+        // $("canvas").css("transform", "scale(2.5)").fadeOut(500);
+        // $(".circle").fadeOut();
+        // $(".level-1-game").css({
+        //     "display": "flex",
+        //     "align-items": "center",
+        //     "justify-content": "center",
+        // });
+        if($("div.intro-banner-vdo-play-btn.pinkBg").hasClass("intro-banner-vdo-play-btn pinkBg")){
             console.log("foo");
 
             // Highlighting the next level after completing the current level
-            // $("li.ball.bubble").removeClass("ball bubble").next().addClass("ball bubble");
+            $("div.intro-banner-vdo-play-btn.pinkBg").removeClass("intro-banner-vdo-play-btn pinkBg");
+            
+            $(this).next().closest('.circle-position').children('.circle').addClass("intro-banner-vdo-play-btn pinkBg").prepend("<span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span><span class=\"ripple pinkBg\"></span>");
+            console.log("bar");
+            
 
         } else {
             // Initial stage after reloads
